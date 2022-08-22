577e96245904d2a3b26c6a2a59b9ff9afb529ca8
sharjeel-193 <msmaqsood.bscs19seecs@seecs.edu.pk>
2022-08-22 17:40:01 +0500

    Images

--- frontend/tickup/sections/AddImages.js
+++ frontend/tickup/sections/AddImages.js
@@ -1,81 +1,90 @@
-import React from 'react'
-import {Box, TextField, Grid, Button} from '@mui/material'
-import { useFormik } from 'formik';
-import * as yup from 'yup';
+import React, { useState } from 'react'
+import {Box, TextField, Grid, Button, Typography, colors} from '@mui/material'
 
-const SUPPORTED_FORMATS = ["image/jpg", "image/jpeg", "image/gif", "image/png"];
 
-export const validateImageType = (value) => {
-  if(value) {
-    let type = value.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/)[0]
-    return SUPPORTED_FORMATS.includes(type)
-  }
-}
-
-const validationSchema = yup.object({
-    file: yup.mixed(),
-});
 
 function AddImages(props) {
     const {handleBack, handleComplete, activeStep, checkLastToFill} = props
-    const formik = useFormik({
-        initialValues: {
-            file: {}
-        },
-        validationSchema: validationSchema,
-        onSubmit: (values) => {
-            
-            handleComplete(values)
-        },
-    });
+    const [image, setImage] = useState(null)
+    const [imageUrl, setImageUrl] = useState(null)
+    const [imgUploaded, setImgUploaded] = useState(false)
+
+    const uploadImage = async () => {
+        const data = new FormData()
+        data.append("file",image)
+        data.append("upload_preset","tickup_tour_photos")
+        data.append("cloud_name","tickup")
+        fetch("https://api.cloudinary.com/v1_1/tickup/image/upload",{
+            method: 'POST',
+            body: data
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log({CLoud: data.url})
+            setImageUrl(data.url)
+            setImgUploaded(true)
+        })
+    } 
+
+    const handleSubmit = () => {
+        const values = {
+            file: imageUrl,
+        }
+        handleComplete(values)
+    }
+    // const myImage = cld.image('sample'); 
     return (
         <Box>
-            <form onSubmit={(e) => {
-                e.preventDefault()
-                console.log(e.currentTarget)
-                handleComplete(e.currentTarget, 'multipart')
-            }} encType={'application/x-www-form-urlencoded'}>
-                <TextField 
-                    variant={'filled'} 
-                    type={'file'} 
-                    label={'Images'} 
-                    fullWidth 
-                    placeholder='Image'
-                    InputLabelProps={{
-                        shrink: true                                              
-                    }} 
-                    name={'file'}
-                    value={formik.values.image}
-                    onChange={formik.handleChange}
-                    error={formik.touched.file && Boolean(formik.errors.file)}
-                    helperText={formik.touched.file && formik.errors.file}
-                    accept="image/*"
-                />
-                <Box
-                    display={'flex'}
-                    alignItems={'center'}
-                    justifyContent={'space-between'}
-                    marginTop={'12px'}
+            {/* <AdvancedImage
+			           cldImg={myImage}
+			        /> */}
+            <TextField 
+                variant={'filled'} 
+                type={'file'} 
+                label={'Images'} 
+                fullWidth 
+                placeholder='Image'
+                InputLabelProps={{
+                    shrink: true                                              
+                }} 
+                name={'file'}
+                // value={image}
+                onChange={(e) => setImage(e.target.files[0])}
+                accept="image/*"
+            />
+            <Box
+                textAlign={'center'}
+            >
+            {imgUploaded?
+                <Typography sx={{color: colors.green[600]}}>Image Uploaded  	&#10004;</Typography>:
+                <Button disabled={!image} onClick={uploadImage}>Upload Image</Button>
+            }
+            </Box>
+            
+            <Box
+                display={'flex'}
+                alignItems={'center'}
+                justifyContent={'space-between'}
+                marginTop={'12px'}
+            >
+                <Button 
+                    variant={'outlined'}
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
                 >
-                    <Button 
-                        variant={'outlined'}
-                        disabled={activeStep === 0}
-                        onClick={handleBack}
-                    >
-                        Back
-                    </Button>
-                    <Button 
-                        variant={'outlined'}
-                        // disabled={activeStep === 2}
-                        // onClick={handleComplete}
-                        type={'submit'}
-                    >
-                        {checkLastToFill()
-                            ? 'Finish'
-                            : 'Next'}
-                    </Button>
-                </Box>
-            </form>
+                    Back
+                </Button>
+                <Button 
+                    variant={'outlined'}
+                    onClick={handleSubmit}
+                    disabled={!imageUrl}
+                >
+                    {checkLastToFill()
+                        ? 'Finish'
+                        : 'Next'}
+                </Button>
+            </Box>
+            
         </Box>
 
     )
